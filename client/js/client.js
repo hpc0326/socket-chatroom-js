b4a7d9cb68618adff32cb47a5d790fe943b22dc5
hpc0326 <jack20010326@gmail.com>
2022-11-01 01:43:00 +0800

    first

---
+++ client/js/client.js
@@ -0,0 +1,70 @@
+'use strict'
+
+const userName = document.querySelector('input#username')
+const inputRoom = document.querySelector('input#room')
+const btnConnect = document.querySelector('button#connect')
+const btnLeave = document.querySelector('button#leave')
+const outputArea = document.querySelector('textarea#output')
+const inputArea = document.querySelector('textarea#input')
+const btnSend = document.querySelector('button#send')
+
+let socket
+let room
+
+btnConnect.onclick = () => {
+  socket = io('wss://localhost:4443')
+
+  socket.on('joined', (room, id) => {
+    btnConnect.disabled = true
+    btnLeave.disabled = false
+    inputArea.disabled = false
+    btnSend.disabled = false
+  })
+
+  socket.on('leave', (room, id) => {
+    btnConnect.disabled = false
+    btnLeave.disabled = true
+    inputArea.disabled = true
+    btnSend.disabled = true
+
+    socket.disconnect()
+  })
+
+  socket.on('message', (room, data) => {
+    console.log(room, data)
+    outputArea.scrollTop = outputArea.scrollHeight
+    outputArea.value = outputArea.value + data + '\n'
+  })
+
+  socket.on('disconnect', (reason) => {
+    btnConnect.disabled = false
+    btnLeave.disabled = true
+    inputArea.disabled = true
+    btnSend.disabled = true
+  })
+
+  room = inputRoom.value
+  socket.emit('join', room)
+}
+
+btnSend.onclick = () => {
+  let data = inputArea.value
+  data = userName.value + ':' + data
+  socket.emit('message', room, data)
+  inputArea.value = ''
+}
+
+btnLeave.onclick = () => {
+  room = inputRoom.value
+  socket.emit('leave', room)
+}
+
+inputArea.onkeypress = (event) => {
+  if (event.keyCode === 13) {
+    let data = inputArea.value
+    data = userName.value + ':' + data
+    socket.emit('message', room, data)
+    inputArea.value = ''
+    event.preventDefault()
+  }
+}
---
+++ server.js
@@ -0,0 +1,40 @@
+'use strict'
+
+const https = require('https')
+const fs = require('fs')
+const express = require('express')
+const socketIo = require('socket.io')
+
+const app = express()
+
+const options = {
+  key: fs.readFileSync('key.pem'),
+  cert: fs.readFileSync('cert.pem'),
+}
+
+const https_server = https.createServer(options, app)
+https_server.listen(4443, '0.0.0.0')
+
+const io = socketIo(https_server, {
+  cors: true,
+})
+
+// 收到使用者連線
+io.sockets.on('connection', (socket) => {
+    console.log(socket.id, '已連線')
+  
+    socket.on('message', (room, data) => {
+      io.in(room).emit('message', room, data)
+    })
+  
+    socket.on('join', (room) => {
+      socket.join(room)
+      socket.emit('joined', room, socket.id)
+    })
+  
+    socket.on('leave', (room) => {
+      socket.leave(room)
+      socket.to(room).emit('bye', room, socket.id)
+      socket.emit('leave', room, socket.id)
+    })
+  })
